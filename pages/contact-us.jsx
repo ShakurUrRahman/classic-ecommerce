240267ce372252e811e494b2b33d5b6cb46e976a
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2023-05-02 22:41:18 +0600

    first commit

---
+++ pages/contact-us.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <div>
+      This is contact us <Link href="/">page</Link>
+    </div>
+  );
+};
+
+export default ContactUs;
