d7e6394d93a24b4f16664f1077ad11f82ff0fa00
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2023-05-07 15:47:43 +0600

    added carousale image

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -15,17 +15,10 @@ const Hero = () => {
       url: "https://i.ibb.co/ZSyVBzk/hero1.png",
     },
     {
-      url: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+      url: "https://i.ibb.co/zHSY87P/hero2.png",
     },
     {
-      url: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
-    },
-
-    {
-      url: "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
+      url: "https://i.ibb.co/nspy8fJ/hero3.png",
     },
   ];
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -92,7 +85,7 @@ const Hero = () => {
       <div className="-mt-[448px] h-[480px] py-16 px-4 relative group">
         <div
           style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className="ml-[700px] w-[530px] h-[415px] bg-right bg-no-repeat  duration-500"
+          className="ml-[640px] w-[520px] h-[415px] bg-right bg-no-repeat duration-500"
           alt=""
         />
         {/* Left Arrow */}
@@ -103,14 +96,14 @@ const Hero = () => {
         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactRight onClick={nextSlide} size={30} />
         </div>
-        <div className="flex top-4 justify-center py-2">
+        <div className="flex top-4 justify-center -mt-10">
           {slides.map((slide, slideIndex) => (
             <div
               key={slideIndex}
               onClick={() => goToSlide(slideIndex)}
               className="text-2xl cursor-pointer"
             >
-              {/* <RxDotFilled /> */}
+              <RxDotFilled />
             </div>
           ))}
         </div>
