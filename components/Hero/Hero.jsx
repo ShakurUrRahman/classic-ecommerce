6cf873bec9baaa1291d9ede8dab23229bf140035
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2023-05-15 21:39:36 +0600

    hero processing

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
 import React, { useState } from "react";
 import { AiOutlineRight } from "react-icons/ai";
-import { AiOutlineMenuUnfold } from "react-icons/ai";
 import { FaChevronDown } from "react-icons/fa";
-import { BsChevronCompactLeft } from "react-icons/bs";
-import { BsChevronCompactRight } from "react-icons/bs";
 import { CgProfile } from "react-icons/cg";
 import { HiOutlineShoppingBag } from "react-icons/hi";
 import { MdOutlineFavoriteBorder } from "react-icons/md";
@@ -12,8 +8,8 @@ import { IoIosGitCompare } from "react-icons/io";
 import { RxDotFilled } from "react-icons/rx";
 import { BsSearch } from "react-icons/bs";
 import { HiOutlineChevronUpDown } from "react-icons/hi2";
-import hero1 from "../../public/assets/hero/hero1.png";
-import logo from "../../public/assets/hero/logo.jpg";
+import { AiOutlineMinus } from "react-icons/ai";
+import Link from "next/link";
 
 const Hero = () => {
   const menuList = [
@@ -49,29 +45,28 @@ const Hero = () => {
   ];
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex);
   };
 
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleCombinedClick = (slideIndex) => {
+    handleClick();
+    goToSlide(slideIndex);
+  };
+
   return (
-    <section>
+    <section className="bg-gray-200">
       <nav className="h-[47px] px-[65px] bg-[#FED700] flex items-center justify-between">
         <div className="flex justify-between gap-[100px]">
           <div className="flex gap-4 items-center">
             <p className="font-bold text-sm">Shop By Department</p>
-            <p className="mt-[5px] text-[10px] text-gray-500">
+            <p className="mt-[5px] text-[10px]">
               <FaChevronDown />
             </p>
           </div>
@@ -113,43 +108,44 @@ const Hero = () => {
           </div>
         </div>
       </nav>
-      <div className="px-[42px] flex flex-row">
-        <menu className="py-2 border-t-0 border-2 border-[#FED700] rounded-t-none rounded-md basis-[22%]">
-          <ul className="flex py-1 px-4 flex-col gap-[8px] text-[14px] content-center">
-            {menuList?.map((menu, index) => (
-              <li
-                key={index}
-                className={`border-t-0 border-b-2 pb-1 border-gray-100 flex justify-between items-center ${
-                  menu.bold === "yes" ? "font-bold" : "font-normal"
-                }`}
-              >
+      <div className="px-[42px] flex justify-between">
+        <menu className="pt-3 px-2 bg-white flex flex-col gap-[8px] text-[14px] basis-[23%] border-t-0 border-2 border-[#FED700] rounded-t-none rounded-md">
+          {menuList?.map((menu, index) => (
+            <Link
+              key={index}
+              href="#"
+              className={`hover:bg-gray-100 border-t-0 border-b-2 border-gray-100 ${
+                menu.bold === "yes" ? "font-bold" : "font-normal"
+              }`}
+            >
+              <div className="px-3 flex justify-between items-center hover:font-bold py-[1.5px]">
                 {menu.list}
                 {menu.bold ? (
                   ""
                 ) : (
-                  <AiOutlineRight className="text-gray-500 text-xs" />
+                  <AiOutlineRight className="text-gray-500 text-xs hover:text-base" />
                 )}
-              </li>
-            ))}
-          </ul>
-        </menu>
-      </div>
-      <div className="-mt-[712px] h-[480px] py-16 px-4 relative group duration-300">
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className="ml-[700px] w-[520px] h-[415px] bg-right bg-no-repeat duration-500"
-          alt=""
-        />
-        <div className="flex top-4 justify-center -mt-10">
-          {slides.map((slide, slideIndex) => (
-            <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className="text-2xl cursor-pointer"
-            >
-              <RxDotFilled />
-            </div>
+              </div>
+            </Link>
           ))}
+        </menu>
+        <div className="basis-[74%]">
+          <div
+            style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+            className="w-[520px] h-[450px] bg-right bg-no-repeat duration-500 ml-[350px]"
+            alt=""
+          />
+          <div className="flex">
+            {slides.map((slide, slideIndex) => (
+              <div
+                key={slideIndex}
+                onClick={() => handleCombinedClick(slideIndex)}
+                className="text-2xl cursor-pointer -mt-10 text-slate-300 hover:text-slate-500 inline-flex items-center transition-colors"
+              >
+                {isActive ? <RxDotFilled /> : <AiOutlineMinus />}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
